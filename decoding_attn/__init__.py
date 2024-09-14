88f856b767cb713282e6e179432c85f9af8ca7ae
Bruce_Lee_LY <yong-li14@tsinghua.org.cn>
2024-09-15 07:57:00 +0800

    first commit

---
+++ decoding_attn/__init__.py
@@ -0,0 +1,19 @@
+# Copyright 2023. All Rights Reserved.
+# Author: Bruce-Lee-LY
+# Date: 21:14:13 on Tue, Oct 31, 2023
+#
+# Description: decoding attn init
+
+# !/usr/bin/python3
+# coding=utf-8
+
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import with_statement
+
+__version__ = "0.1.0"
+
+from decoding_attn.decoding_attn_interface import (
+    decoding_attn_fwd,
+)
